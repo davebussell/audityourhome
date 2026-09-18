2f9470f4574bcf73800895330d49dc5f59cae6c8
Dave <dave@clickshift.ca>
2026-09-18 11:04:51 -0400

    Site update 2026-09-18 11:04:51.24

--- PUSH-TO-GITHUB.bat
+++ PUSH-TO-GITHUB.bat
@@ -1,6 +1,7 @@
 @echo off
 cd /d "%~dp0"
 echo Publishing your changes to audityourhome.com ...
+git rm -r -q --cached --ignore-unmatch ayh-update-v2.zip ayh-v3.zip _to_delete 2>nul
 git add -A
 git commit -m "Site update %date% %time%"
 git push -u origin main
